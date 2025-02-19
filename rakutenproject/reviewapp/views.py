569cfc0ade225179cdc56b8ee381dd78e871ed97
しろあり <siromk175@gmail.com>
2025-02-19 15:15:43 +0900

    ダッシュボード作成

--- rakutenproject/reviewapp/views.py
+++ rakutenproject/reviewapp/views.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import ReviewModel
+
 
 # Create your views here.
 def hello_world(request) :
     return HttpResponse('<h2>hello world from Review_app!!!</h2>')
+
+# LoginRequiredMixin:要認証
+class DashBoardView(LoginRequiredMixin, ListView) :
+    template_name = 'dashboard.html'
+    model = ReviewModel
+
