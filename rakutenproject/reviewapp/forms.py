03ba49001301fdb8926b239bd47dda227b4c6142
しろあり <siromk175@gmail.com>
2025-03-11 15:29:45 +0900

    フォーム作成途中

--- rakutenproject/reviewapp/forms.py
+++ rakutenproject/reviewapp/forms.py
@@ -8,13 +8,14 @@ from .models import ReviewModel
 class ReviewForm(forms.Form):
     name_rak_url = forms.URLField(
         label='楽天レビューURLを入力' ,
-        widget=forms.URLInput(attrs={'class': ''}),
+        widget=forms.URLInput(attrs={'class': 'form-control mt-3'}),
         max_length=1000 ,
-
     )
     name_rak_page = forms.IntegerField(
         label='取得するページ数' ,
-        widget=forms.NumberInput(attrs={'class': 'form-label d-inline'}) ,
+        widget=forms.NumberInput(attrs={'class': 'form-control mt-3 w-25'}) ,
         min_value=1 ,
         max_value=50 ,
     )
+
+
