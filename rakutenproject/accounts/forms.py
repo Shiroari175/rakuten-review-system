b3ac46318e9f67d16a6b8dd9dd50ad7ee53ad6a3
しろあり <siromk175@gmail.com>
2025-03-18 11:31:30 +0900

    アカウント登録CSS見直し

--- rakutenproject/accounts/forms.py
+++ rakutenproject/accounts/forms.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+
+    # attrsのform-controlを反映させるために、直接指定する
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control mt-1 mb-3'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control mt-1 mb-3'})
+    )
+
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
-
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control mt-1 mb-3'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mt-1 mb-3'}),
+            # 'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            # 'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
