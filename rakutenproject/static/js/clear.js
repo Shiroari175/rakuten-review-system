0f20d5e50f4d09e68307e516a6f0808a0040c9fe
しろあり <siromk175@gmail.com>
2025-03-19 15:06:17 +0900

    データ集計・分析機能作成

--- rakutenproject/static/js/clear.js
+++ rakutenproject/static/js/clear.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
     $('#clearButton').click(function() {
         // テキスト入力フィールドをクリア
-        //$('input[type="text"]').val('');
         $('#id-item_nm').val('');
-
         // ラジオボタンの選択をクリア
         $('input[type="radio"]').prop('checked', false);
-//        $('#id-rating1').prop('checked', false);
-//        $('#id-rating2').prop('checked', false);
-//        $('#id-rating3').prop('checked', false);
-//        $('#id-rating4').prop('checked', false);
-//        $('#id-rating5').prop('checked', false);
-
-
-
     });
 });
 
